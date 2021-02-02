f11b14034ffae3bae553154efd9e7e088bf089e5
Karan Kangude <karankangude17@gmail.com>
2021-02-02 13:49:30 +0530

    login route created

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     'mongodb+srv://immortal:Andromeda%40123@devconnector.skjxw.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  secret: 'SECRET',
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 // Load user
 const User = require('../../models/User');
-// @router  /api/users/test
+// @router  GET /api/users/test
 // @desc    test route for users
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
-// @router  /api/users/register
+// @router  POST /api/users/register
 // @desc    registration route for users
 // @access  Public
 router.post('/register', (req, res) => {
@@ -42,6 +44,51 @@ router.post('/register', (req, res) => {
           });
         });
       }
+    })
+    .catch((err) => console.log(err));
+});
+
+// @router  POST /api/users/login
+// @desc    login for users
+// @access  Public
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ email: 'User not found!' });
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((isMatch) => {
+          if (isMatch) {
+            //user matched
+
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar,
+            };
+            //sign token
+            jwt.sign(
+              payload,
+              keys.secret,
+              { expiresIn: 36000 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token,
+                });
+              }
+            );
+          } else {
+            return res.status(400).json({ password: 'password incorrect' });
+          }
+        })
+        .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
 });
